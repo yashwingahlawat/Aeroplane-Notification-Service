67700625e209144674d109ae34ad13ffbd9f6b2c
yashwingahlawat <yashwingahlawat@gmail.com>
2025-07-27 04:48:04 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express=require('express')
+
+const {ServerConfig,Logger}=require('./config')
+
+const apiRoutes=require('./routes')
+
+const app=express()
+
+app.use('/api',apiRoutes)
+
+app.listen(ServerConfig.PORT,()=>{
+    console.log(`Server running on ${ServerConfig.PORT}`)
+    Logger.info(`Successfully started the server`,'root',{})
+})
